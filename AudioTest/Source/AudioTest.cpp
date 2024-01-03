9220348b92cf58252edad7af92193e6309c2cff3
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2024-01-02 20:49:08 -0700

    Write some tests....which I'll try tomorrow.

--- AudioTest/Source/AudioTest.cpp
+++ AudioTest/Source/AudioTest.cpp
@@ -1,23 +1,34 @@
 #include "WaveFormat.h"
 #include "ByteStream.h"
 #include "AudioSink.h"
+#include "WaveForm.h"
+#include "AudioTest.h"
 #include <iostream>
 
 using namespace AudioDataLib;
 
 int main(int argc, char** argv)
 {
+	TestWaveFormat();
+	//TestWaveForm();
+	//TestAudioSink();
+
+	return 0;
+}
+
+bool TestAudioSink()
+{
+	bool success = false;
+	std::string error;
+
 	WaveFormat waveFormat;
 
 	AudioData* audioDataA = nullptr;
 	AudioData* audioDataB = nullptr;
-	
+
 	FileInputStream inputStreamA("TestData/TestAudio1.wav");
 	FileInputStream inputStreamB("TestData/TestAudio2.wav");
 	FileOutputStream outputStream("TestData/TestAudioResult.wav");
-
-	std::string error;
-	bool success = false;
 
 	do
 	{
@@ -50,5 +61,72 @@ int main(int argc, char** argv)
 	delete audioDataA;
 	delete audioDataB;
 
-	return success ? 0 : -1;
+	return success;
+}
+
+bool TestWaveForm()
+{
+	WaveFormat waveFormat;
+
+	AudioData* audioDataIn = nullptr;
+	FileInputStream inputStream("TestData/TestAudio1.wav");
+
+	std::string error;
+	bool loadedWave = waveFormat.ReadFromStream(inputStream, audioDataIn, error);
+	assert(loadedWave);
+
+	uint64_t audioBufferSize = audioDataIn->GetAudioStream()->GetSize();
+	uint8_t* audioBuffer = new uint8_t[(uint32_t)audioBufferSize];
+	uint64_t numBytesRead = audioDataIn->GetAudioStream()->ReadBytesFromStream(audioBuffer, audioBufferSize);
+	assert(numBytesRead == audioBufferSize);
+
+	WaveForm waveForm;
+	waveForm.ConvertFromAudioBuffer(audioDataIn->GetFormat(), audioBuffer, audioBufferSize, 0);
+
+	::memcpy(audioBuffer, 0, (size_t)audioBufferSize);
+
+	waveForm.ConvertToAudioBuffer(audioDataIn->GetFormat(), audioBuffer, audioBufferSize, 0);
+
+	AudioData* audioDataOut = new AudioData();
+	uint64_t numBytesWritten = audioDataOut->GetAudioStream()->WriteBytesToStream(audioBuffer, audioBufferSize);
+	assert(numBytesWritten == audioBufferSize);
+
+	FileOutputStream outputStream("TestData/TestAudio1_copy.wav");
+	bool savedWave = waveFormat.WriteToStream(outputStream, audioDataOut, error);
+	assert(savedWave);
+
+	delete audioDataIn;
+	delete audioDataOut;
+	delete[] audioBuffer;
+
+	return true;
+}
+
+bool TestWaveFormat()
+{
+	WaveFormat waveFormat;
+
+	AudioData* audioDataIn = nullptr;
+	FileInputStream inputStream("TestData/TestAudio1.wav");
+
+	std::string error;
+	bool loadedWave = waveFormat.ReadFromStream(inputStream, audioDataIn, error);
+	assert(loadedWave);
+
+	AudioData* audioDataOut = new AudioData();
+	while (audioDataIn->GetAudioStream()->CanRead())
+	{
+		uint8_t byte = 0;
+		audioDataIn->GetAudioStream()->ReadBytesFromStream(&byte, 1);
+		audioDataOut->GetAudioStream()->WriteBytesToStream(&byte, 1);
+	}
+
+	FileOutputStream outputStream("TestData/TestAudio1_copy.wav");
+	bool savedWave = waveFormat.WriteToStream(outputStream, audioDataOut, error);
+	assert(savedWave);
+
+	delete audioDataIn;
+	delete audioDataOut;
+
+	return true;
 }
---
+++ AudioTest/Source/AudioTest.h
@@ -0,0 +1,5 @@
+#pragma once
+
+bool TestAudioSink();
+bool TestWaveForm();
+bool TestWaveFormat();
