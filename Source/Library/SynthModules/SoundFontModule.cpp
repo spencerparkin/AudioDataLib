4931857988fc02ac2bd69c328074cd7d288acaf6
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2024-01-25 19:55:34 -0700

    Note.

--- Source/Library/SynthModules/SoundFontModule.cpp
+++ Source/Library/SynthModules/SoundFontModule.cpp
@@ -17,5 +17,11 @@ SoundFontModule::SoundFontModule()
 	//       perhaps by over or under-sampling the recording.  (A recording played back faster sounds
 	//       higher in pitch, or played back slower sounds lower in pitch.)
 
+	// TODO: Maybe this module should not know anything about interpolation of pitch shifting.  Maybe all
+	//       it should do is just know how to play back an assigned sample and loop it without artifacting.
+	//       Some other module can receive the sound and pitch shift it as desired, or two instances of this
+	//       module could be fed into an interpolating module to get the correct pitch.  In any case, making
+	//       this module as simple as possible is helpful and an easy first step.
+
 	return false;
 }
