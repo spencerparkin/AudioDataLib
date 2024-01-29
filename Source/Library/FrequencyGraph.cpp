b0bd428190077908a9d53efb089ce1d46ec549c9
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2024-01-28 23:37:32 -0700

    Give up, for now, on an analytical approach to determining the perceived pitch of a given sound sample.  It is extremely non-trivial, apparently.  I think the FFT helps, but I don't know where to go from there, and researching this topic is overwhelming.

--- Source/Library/FrequencyGraph.cpp
+++ Source/Library/FrequencyGraph.cpp
@@ -66,6 +66,19 @@ bool FrequencyGraph::ToWaveForm(WaveForm& waveForm, Error& error) const
 	return false;
 }
 
+// TODO: I've learned just now that the "dominant frequency" as I've defined it here,
+//       is not necessarily (and probably *not* in most cases) the *percieved* pitch of
+//       a given sound sample.  It is sometimes the GCD of the dominant frequencies
+//       above some threshold, I think, and is called the residue pitch, but this is
+//       not always the case.  Note that this is sometimes a pitch not present at all
+//       as showing up as a spike in the graph!  A few minutes of trying to research
+//       this topic has been overwelming.  I could try pinning down the perceived pitch
+//       of all my samples and then look for patterns in the graph data, but I think
+//       that would probably be a waste of time.  (Like me writing down prime numbers
+//       and then seeing if I could find a pattern in those!)  In the end, I may just
+//       have to determine the perceived pitches of all the font-data samples by ear,
+//       because I just have no idea how to calculate it analytically or get it from
+//       the font-data itself (which is ridiculous, by the way.)
 double FrequencyGraph::FindDominantFrequency(double* dominantStrength /*= nullptr*/) const
 {
 	Plot dominantPlot{ 0.0, 0.0 };
