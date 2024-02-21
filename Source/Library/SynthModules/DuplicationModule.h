47147cac53ed1ffac996fcf60dbafcca0e695f99
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2024-02-21 01:22:05 -0700

    You're up way too late.  :(

--- Source/Library/SynthModules/DuplicationModule.h
+++ Source/Library/SynthModules/DuplicationModule.h
@@ -7,9 +7,9 @@ namespace AudioDataLib
 	// This module duplicates the signal it's given so that it can be passed
 	// on to other modules.  In general, (and this may be thought of as a
 	// limitation of my design), no two distinct modules can depend on a
-	// single module.  However, this module is designed to overcome that
-	// limitation by simply being inserted between the desired module and all
-	// those other modules that we want to depend upon it.
+	// single module (except for this one!)  This module is designed to overcome
+	// that limitation by simply being inserted between the desired module and
+	// all those other modules that we want to depend upon it.
 	class AUDIO_DATA_LIB_API DuplicationModule : public SynthModule
 	{
 	public:
